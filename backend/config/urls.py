a5b65b230b19b080c99bc9df0b2c3c56b99df6f6
romeomanoela <romeomanoela18@gmail.com>
2025-05-24 15:19:33 +0300

    basic configurations

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for a config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from backend.config import settings
+from backend.config.settings import DEBUG
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+if DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
